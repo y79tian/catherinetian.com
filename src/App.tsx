3692ef3ffbe472722d42cd20270bb4f3ecead54c
y79tian <y79tian@uwaterloo.ca>
2024-01-01 20:19:32 -0500

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import logo from './logo.svg';
-import './App.css';
+import Home from './views/home/home';
 
-function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+  const renderRoutes = () => (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+  return <>{renderRoutes()}</>;
+};
 
 export default App;
