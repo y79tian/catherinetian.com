cdf40515ff5a173924e3a5da5be013efc612f100
y79tian <y79tian@uwaterloo.ca>
2024-06-05 13:40:43 -0400

    update the resume

--- src/theme/links.ts
+++ src/theme/links.ts
@@ -1,6 +1,6 @@
 const Links = {
   resume:
-    'https://docs.google.com/document/d/1PDl2uPj8xCVCzNUG40DTclPpD4LhvI5JJKZVa9g3tg4/edit',
+    'https://docs.google.com/document/d/10Ouara56934g9TBI2Wg68ETvOWVzzuoMTcji20UsCDg/edit?usp=sharing',
 } as const;
 
 export type ThemeLinks = typeof Links;
