6cc81775e2085ea20f00645027544437aefcb280
y79tian <y79tian@uwaterloo.ca>
2024-06-09 19:48:38 -0400

    add progress indicator

--- src/shared/page/page.tsx
+++ src/shared/page/page.tsx
@@ -1,8 +1,10 @@
-import { PropsWithChildren, useEffect, useRef, useState } from 'react';
+import { useScroll, useSpring } from 'framer-motion';
+import { PropsWithChildren, useRef } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { useTheme } from 'styled-components';
 
 import useKeyPress from '../../hooks/useKeyPress';
+import useScrollPosition from '../../hooks/useScrollPosition';
 import { Footer } from '../footer/footer';
 import Navbar from '../navbar/navbar';
 import { pathMap, viewsMetadata } from '../navbar/navbar.metadata';
@@ -25,9 +27,16 @@ export const Page = ({
 }: PageProps) => {
   const theme = useTheme();
   const containerRef = useRef<HTMLDivElement>(null);
-  const [position, setPosition] = useState<'before' | 'after' | null>(null);
   const { pathname } = useLocation();
   const navigate = useNavigate();
+  const { scrollYProgress } = useScroll({
+    container: containerRef,
+  });
+  const yProgress = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001,
+  });
 
   const onNavigate = (isPrev: boolean) => {
     const currentIndex = pathMap.get(pathname);
@@ -44,28 +53,7 @@ export const Page = ({
     }
   };
 
-  useEffect(() => {
-    const handleScroll = () => {
-      if (containerRef.current) {
-        const container = containerRef.current;
-        const rect = container.getBoundingClientRect();
-        const scrollPosition = container.scrollTop + rect.top;
-        const containerMidpoint = rect.top + rect.height / 2;
-
-        if (scrollPosition < containerMidpoint) {
-          setPosition('before');
-        } else {
-          setPosition('after');
-        }
-      }
-    };
-    const container = containerRef.current;
-    container?.addEventListener('scroll', handleScroll);
-
-    return () => {
-      container?.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
+  const position = useScrollPosition(containerRef);
   const scrollToTop = () =>
     containerRef.current?.scrollTo({ top: 0, behavior: 'smooth' });
   const scrollToBottom = () =>
@@ -92,7 +80,7 @@ export const Page = ({
   };
   return (
     <PageContainer ref={containerRef} $canScroll={canScroll}>
-      {showNavBar && <Navbar />}
+      {showNavBar && <Navbar canScroll={canScroll} yProgress={yProgress} />}
       {children}
       <Footer showSocialMedias={showSocialMedias} />
       {canScroll && renderScrollButton()}
