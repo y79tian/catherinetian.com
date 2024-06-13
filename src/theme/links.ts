64fa53675661c36d9e6e81d4b964aba60add1f48
y79tian <y79tian@uwaterloo.ca>
2024-06-12 22:39:21 -0400

    add new links

--- src/theme/links.ts
+++ src/theme/links.ts
@@ -8,6 +8,9 @@ const Links = {
     'https://www.youtube.com/watch?v=T8IOJyI7NaE&ab_channel=DoveBennett',
   rocketIntro: 'https://x.com/RocketVideos3',
   xonicIntro: 'https://xonicgolf.com/',
+  golfProject: 'https://www.youtube.com/embed/PVuhYG_10vo',
+  rocketProject: 'https://www.youtube.com/embed/VuI70Xner5A',
+  satoshiProject: 'https://www.youtube.com/embed/nY1b0u9N-rA',
 } as const;
 
 export type ThemeLinks = typeof Links;
